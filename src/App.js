0da73b43669efce1207ea4ccda098eca0bc54a15
xaviguasch <xaviguasch@gmail.com>
2022-01-12 23:28:34 +0100

    added Toggle component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react'
 
 import InfoCard from './components/InfoCard'
 import Search from './components/Search'
+import Toggle from './components/Toggle'
 
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Github Jobs</h1>
+      <h1>devfinder</h1>
+      <Toggle />
       <Search />
       <InfoCard />
     </div>
---
+++ src/components/Toggle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Toggle.css'
+
+const Toggle = () => {
+  return (
+    <div className='Toggle'>
+      <h2>Toggle</h2>
+    </div>
+  )
+}
+
+export default Toggle
